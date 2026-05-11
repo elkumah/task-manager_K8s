03b29590275a16e48313f3375229c34a0906cab3
elkumah <e.fkumah@gmail.com>
2026-05-11 21:49:38 +0000

    Containerize frontend app to communicate with Backend api

--- frontend/src/components/TodoApp.jsx
+++ frontend/src/components/TodoApp.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://backend-todo:5000/api/todos";
+const API_URL = "http://localhost:5000/api/todos";
 
 const TodoApp = () => {
   const [todos, setTodos] = useState([]);
